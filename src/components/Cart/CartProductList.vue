470bc13c1cde55d1c641354e5d0f465125c674ad
GitKnda <mgautiez59@gmail.com>
2025-01-16 14:03:20 +0100

    first commit

---
+++ src/components/Cart/CartProductList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import CartProduct from "./CartProduct.vue";
+
+defineProps({
+  cartList: Array,
+});
+</script>
+
+<template>
+  <div>
+    <CartProduct v-for="cart of cartList" :cart="cart"/>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
